6de8f5a55cf51710233fc398805e06e01a643777
andrewimpellitteri <andrewimpellitteri@gmail.com>
2023-12-19 21:40:12 -0500

    llm done

--- profileSearch/search.py
+++ profileSearch/search.py
@@ -1,22 +1,26 @@
 import pandas as pd
 from llama_cpp import Llama
 import re
+from tqdm import tqdm
 # Create an empty DataFrame
-df = pd.DataFrame(columns=["Item", "Profile", "mgs"])
+df = pd.DataFrame(columns=["Item", "Profile"])
+
 
 def extract_product_name(input_str):
     # Define a regular expression pattern to match the prefixes and patterns to remove
-    pattern = r'\b(SUB|SALT|\d+ML|\d+MG)\b'  # Updated pattern
+    pattern = r'\b(\d+ML|\d+MG)\b'  # Updated pattern
 
     # Use re.sub to remove the matched prefixes and patterns
     cleaned_name = re.sub(pattern, '', input_str, flags=re.IGNORECASE).strip()
-
-    # Split the cleaned name by spaces and remove the last word
+    
+    remove_pattern_regex = r'\b[A-Z]+[0-9]+\b'
+    # # Split the cleaned name by spaces and remove the last word
     cleaned_name_parts = cleaned_name.split()
     if len(cleaned_name_parts) > 1:
-        cleaned_name_parts.pop()  # Remove the last word
+        if re.match(remove_pattern_regex, cleaned_name_parts[-1]):
+            cleaned_name_parts.pop()  # Remove the last word
 
-    return ' '.join(cleaned_name_parts)
+    return " ".join(cleaned_name_parts)
 
 
 names = pd.read_excel("TVP_Flavor_List.xls")
@@ -27,7 +31,11 @@ names['Item'] = names['Item'].str.replace(r'(\d+MG)', lambda x: x.group(0).lower
 
 names = names['Item'].tolist()
 
-# model = "/Users/andrew/Documents/dev/text-generation-webui/models/Wizard-Vicuna-7B-Uncensored.Q4_K_M.gguf"
+names = [extract_product_name(name) for name in names]
+
+print(len(names))
+names = list(set(names))
+print(len(names))
 
 model = "/Users/andrew/Documents/dev/text-generation-webui/models/mistral-7b-instruct-v0.2.Q4_K_M.gguf"
 
@@ -46,15 +54,13 @@ llm = Llama(model_path=model, use_mlock=True)
 # if not disp:
 #     names = [extract_product_name(name) for name in names]
 
-# print(len(names))
-# names = list(set(names))
-# print(len(names))
 
 
-for name in names:
+
+for name in tqdm(names, desc='items'):
     example = " (for example 7 Daze Fusion Kiwi Passionfruit Guava should give a response of Kiwi, Passionfruit, Guava)"
 
-    if_unsure = "If you are unsure what then simply reply with: 'none'"
+    if_unsure = "If you are unsure what then reply only with one word: 'none'"
     input_q = f"Describe the flavor profile of this product using only single words seperated by commas: {name}. {example}. {if_unsure}"    
     prompt = f"[INST] <<SYS>> You are a helpful, respectful and honest assistant. Be short and succinct and do not start with any description. Always answer as helpfully as possible. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information. <</SYS>>{input_q}[/INST]"
     
@@ -75,6 +81,9 @@ for name in names:
     answer = answer.strip('.')
     answer = answer.strip("  ")
     answer = answer.strip("\n")
+
+    if "none" in answer.lower():
+        answer = "none"
 
     df = pd.concat([df, pd.DataFrame({"Item": [name], "Profile": [answer]})], ignore_index=True)
 
