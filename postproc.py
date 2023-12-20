09912b1190b5460d761ef3a152e846d6fe4330f5
andrewimpellitteri <andrewimpellitteri@gmail.com>
2023-12-19 22:00:58 -0500

    update index

--- postproc.py
+++ postproc.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import re
 
-df = pd.read_csv("new_profiles.csv")
+df = pd.read_csv("new_profiles.csv", index_col=0)
 
 # Function to clean the text
 def clean_text(text):
@@ -11,5 +11,6 @@ def clean_text(text):
 
 # Apply the cleaning function to the 'Items' column
 df['Profile'] = df['Profile'].apply(clean_text)
+
 
 df.to_csv("cleaned_profiles.csv")
