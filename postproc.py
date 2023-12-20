9485ff20ac70aca6ecb827332714058b90a69cb6
andrewimpellitteri <andrewimpellitteri@gmail.com>
2023-12-19 21:59:06 -0500

    post proc

---
+++ postproc.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import re
+
+df = pd.read_csv("new_profiles.csv")
+
+# Function to clean the text
+def clean_text(text):
+    # Remove anything after a '.' or '(' using regular expressions
+    text = re.split(r'\.|\(|->', text)[0]
+    return text.strip()
+
+# Apply the cleaning function to the 'Items' column
+df['Profile'] = df['Profile'].apply(clean_text)
+
+df.to_csv("cleaned_profiles.csv")
